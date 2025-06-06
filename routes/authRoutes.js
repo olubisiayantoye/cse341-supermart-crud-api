ebbd5af41e4c190f122a3a795e41f65a299e669c
olubisiayantoye <bisikingson@gmail.com>
2025-06-06 20:41:55 +0100

    modified:   routes/authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,3 @@
-//routes/authRoutes.js
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
@@ -7,25 +6,23 @@ router.get('/', (req, res) => {
   res.send('Auth service is up. Try /auth/github to login with GitHub.');
 });
 
-// Start GitHub login
+// GitHub login
 router.get('/github', passport.authenticate('github', { scope: ['user:email'] }));
 
-// GitHub callback route
+// GitHub OAuth callback
 router.get(
   '/github/callback',
   passport.authenticate('github', { failureRedirect: '/' }),
   (req, res) => {
-    // Redirect to a frontend page or dashboard
-    res.redirect('/api-docs') 
-
+    // Redirect to your frontend/dashboard after successful login
+   res.redirect(`${process.env.BASE_URL}/api-docs`);
   }
 );
 
-// Logout route
-router.get('/logout', (req, res) => {
+// Logout
+router.get('/logout', (req, res, next) => {
   req.logout(err => {
     if (err) return next(err);
-    res.json({ message: 'Logged out' });
     res.redirect('/');
   });
 });
